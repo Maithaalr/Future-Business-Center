10d96229aca44818b01c437bf2b7f3265910406e
Maithaalr <maithaalremeithi307@gmail.com>
2025-06-01 16:02:10 +0400

    business_dasho

--- business_dashboard.py
+++ business_dashboard.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 # 1. Setup and Logo
 st.set_page_config(page_title="Business Calculator", layout="centered")
